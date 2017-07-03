0021d074b64be69b90528374ac4ad177e3abeb8a
Christoph Baudson <christoph.baudson@rewe-digital.com>
2017-07-03 14:40:02 +0200

    Draft of computer player component

--- app/index.js
+++ app/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import ReactChess from 'Modules/ReactChess';
 import ReduxChess from 'Modules/ReduxChess';
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import PlayerComputer from 'Modules/PlayerComputer';
 import PlayerHuman from 'Modules/PlayerHuman';
+import ReactChessComputerPlayer from 'Modules/ReactChessComputerPlayer';
 
 const store = createStore(ReduxChess, {
     b: new PlayerHuman(),
@@ -17,7 +19,7 @@ store.subscribe(function() {
     if(!state.isGameOver && state.isComputerTurn) {
         setTimeout(() => {
             let domainEvent = state.currentPlayer.createAction(state.fen);
-            if(domainEvent !== null && domainEvent.type === "pieceMoved" && domainEvent.move) {
+            if(domainEvent !== null && domainEvent.type === 'pieceMoved' && domainEvent.move) {
                 console.log('computer player draws');
                 store.dispatch(domainEvent) // Flux = Dispatcher
             }
@@ -30,6 +32,8 @@ store.dispatch({
 });
 
 ReactDOM.render(
-    <ReactChess store={store} />,
+    <Provider store={store}>
+        <ReactChess />
+    </Provider>,
     document.getElementById('chessboard')
 );
